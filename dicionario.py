54a47cb888d16e90582b3c87cb86d82401f12528
Caio Marinho Chiericatti <157981521+caiomero@users.noreply.github.com>
2024-11-19 20:00:17 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,8 @@
+pessoais = {}
+
+nome = input('digite o seu nome:')
+telefone = input('digite o seu telefone de contato:')
+email = input('digite o seu email:')
+
+pessoais[nome] = telefone, email
+print(pessoais)
